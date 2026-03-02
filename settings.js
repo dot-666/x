1538f0ed54f93c37b4bd113307df1c9df7d2bd85
SuprEmE2 <166421298+Vinpink2@users.noreply.github.com>
2026-03-02 10:28:36 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,3 @@
-
 const settings = {
   packname: '',
   author: '‎',
@@ -7,11 +6,15 @@ const settings = {
   ownerNumber: '254792021944', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
-  maxStoreMessages: 20, 
+  maxStoreMessages: 20,
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
-  version: "2.7.2",
+  version: "2.7.1",
   updateZipUrl: "https://github.com/Vinpink2/fx/archive/refs/heads/main.zip",
+  githubToken: process.env.GITHUB_TOKEN || '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',
+  githubOwner: 'Vinpink2',
+  githubRepo: 'fx',
+  githubBranch: 'main',
 };
 
 module.exports = settings;
