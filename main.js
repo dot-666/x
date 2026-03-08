674a4df1abd8d06ef5a7a12617041d255b17c8bb
dot-666 <dotreplit@gmail.com>
2026-03-08 20:18:21 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -53,13 +53,14 @@ const isAdmin = require('./lib/isAdmin');
 const { Antilink } = require('./lib/antilink');
 const { tictactoeCommand, handleTicTacToeMove } = require('./commands/tictactoe');
 const { normalizeJid, compareJids } = require('./lib/jid');
+const { createFakeContact } = require('./lib/fakeContact');
 
 // New imports for console logging
 const moment = require('moment-timezone');
 const lolcatjs = require('lolcatjs');
 
 // Timezone setting
-const timezones = settings.timezone || 'Asia/Kolkata';
+const timezones = settings.timezone || 'Africa/Nairobi';
 
 const _cache = {
     groupMeta: new Map(),
@@ -493,25 +494,6 @@ async function handleMessages(sock, messageUpdate, printLog) {
             '';
         
        
-// fakeQuoted
-
-function createFakeContact(message) {
-    return {
-        key: {
-            participants: "0@s.whatsapp.net",
-            remoteJid: "status@broadcast",
-            fromMe: false,
-            id: "JUNE-X"
-        },
-        message: {
-            contactMessage: {
-                vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:JUNE MD\nitem1.TEL;waid=${message.key.participant?.split('@')[0] || message.key.remoteJid.split('@')[0]}:${message.key.participant?.split('@')[0] || message.key.remoteJid.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
-            }
-        },
-        participant: "0@s.whatsapp.net"
-    };
-}
-
 const fake = createFakeContact(message);
 
 
