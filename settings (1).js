8c211723ed773d2ab51572420dd4884a6c3b084e
dot-666 <dotreplit@gmail.com>
2026-03-13 18:43:09 +0300

    Add files via upload

---
+++ settings (1).js
@@ -0,0 +1,25 @@
+
+/**
+* supreme
+* caspar x
+*/
+
+const settings = {
+  packname: '✮⃝𝐒ᵘᵖʳᵉᵐᵉ',
+  author: '✮⃝𝐒ᵘᵖʳᵉᵐᵉ',
+  botName: "June-X Bot",
+  botOwner: '✮⃝𝐒ᵘᵖʳᵉᵐᵉ', // Your name
+  ownerNumber: '254792021944', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.7.5",
+  updateBranch: 'main',
+  updateZipToken: '',
+  updateZipUrl: "https://github.com/dot-666/x/archive/refs/heads/main.zip",
+  timezone: 'Africa/Nairobi',
+};
+
+module.exports = settings;
